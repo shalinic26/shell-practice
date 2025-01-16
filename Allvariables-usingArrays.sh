dbc9683eaa3b9267e9fe6da8563162e9603b0462
shalinic26 <shalinichintala@gmail.com>
2025-01-16 20:08:05 +0530

    All variables in an array

--- Allvariables-usingArrays.sh
+++ Allvariables-usingArrays.sh
@@ -10,14 +10,14 @@ echo "The Second tool name: ${TOOLS[1]}"
 
 echo "The Third tool name: ${TOOLS[2]}"
 
-echo "The First tool name: ${CLOUDPROVIDERS[0]}"
+echo "The First cloud provider is: ${CLOUDPROVIDERS[0]}"
 
-echo "The First tool name: ${CLOUDPROVIDERS[1]}"
+echo "The Second cloud provider is: ${CLOUDPROVIDERS[1]}"
 
-echo "The First tool name: ${CLOUDPROVIDERS[2]}"
+echo "The Third cloud provider is: ${CLOUDPROVIDERS[2]}"
 
-echo "Print all the tools available in the list: ${TOOLS[@]}"
+echo "Print all the tools available in the list: ${TOOLS[@]}" # o/p-> is displayed as Terraform Docker Kubernetes in single line
 
 echo "Print all the cloud providers available in the list: ${CLOUDPROVIDERS[@]}"
 
-echo "Output the total number of tools in list: ${TOOLS[#]}"
+echo "Output the total number of tools in list: $#{TOOLS}"
