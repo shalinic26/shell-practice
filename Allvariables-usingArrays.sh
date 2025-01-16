01c0490bc696ceba3cbb159369638b06e57e8317
shalinic26 <shalinichintala@gmail.com>
2025-01-16 20:06:00 +0530

    All variables in an array

--- Allvariables-usingArrays.sh
+++ Allvariables-usingArrays.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-TOOLS=('Terraform', 'Docker', 'Kubernetes')
+TOOLS=('Terraform' 'Docker' 'Kubernetes') # first learning to me in arrays of shell we should NOT be keeping , 
 
-CLOUDPROVIDERS=('AWS', 'Azure', 'Google')
+CLOUDPROVIDERS=('AWS' 'Azure' 'Google')
 
-echo "The first tool name: $TOOLS[0]"
+echo "The First tool name: ${TOOLS[0]}" #2nd learning is while getting the array variable need to use ${}
 
-echo "Print all the tools available in the list: $TOOLS[@]"
+echo "The Second tool name: ${TOOLS[1]}"
 
-echo "Output the total number of tools in list: $TOOLS[#]"
+echo "The Third tool name: ${TOOLS[2]}"
+
+echo "The First tool name: ${CLOUDPROVIDERS[0]}"
+
+echo "The First tool name: ${CLOUDPROVIDERS[1]}"
+
+echo "The First tool name: ${CLOUDPROVIDERS[2]}"
+
+echo "Print all the tools available in the list: ${TOOLS[@]}"
+
+echo "Print all the cloud providers available in the list: ${CLOUDPROVIDERS[@]}"
+
+echo "Output the total number of tools in list: ${TOOLS[#]}"
